b90b1232694788d9454717a50d7dcb0079fec87a
unnikrishnan <unnikottamam@gmail.com>
2021-05-12 12:23:57 -0700

    updated the changes into the join us page

--- wp-content/themes/capitalbike/page.php
+++ wp-content/themes/capitalbike/page.php
@@ -12,10 +12,60 @@
  * @package capitalbike
  */
 
-get_header(); ?>
+get_header();
 
+if (have_rows('page_banner')) {
+  while (have_rows('page_banner')) {
 
-<section id="section_2" class="pt-6 commsec">
+    the_row();
+    $overlay = get_sub_field('overlay_type');
+    ?>
+<section id="section_1" class="pagebanner text-white cloud<?php echo $overlay; ?>">
+    <?php
+    $image = get_sub_field('bg_image');
+    if ($image) {
+
+      $src = aq_resize($image['url'], 1366, 768, true, true, true);
+      $alt = $image['alt'] ? $image['alt'] : get_the_title();
+      ?>
+    <div class="pagebanner__img cloud<?php echo $overlay; ?>">
+        <img src="<?php echo $src; ?>" alt="<?php echo $alt; ?>">
+    </div>
+    <?php
+    }
+    ?>
+    <div class="container">
+        <div class="row pagebanner__row pagebanner-full">
+            <div class="col-md-6">
+                <?php
+                the_sub_field('contents');
+                if (have_rows('buttons')) {
+                  echo '<ul class="ctalist">';
+                  while (have_rows('buttons')) {
+                    the_row();
+                    if (get_sub_field('link') && get_sub_field('title')) { ?>
+                <li>
+                    <a href="<?php the_sub_field(
+                      'link'
+                    ); ?>" class="btn btn-<?php the_sub_field(
+  'color'
+); ?>"><?php the_sub_field('title'); ?></a>
+                </li>
+                <?php }
+                  }
+                  echo '</ul>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+  }
+}
+?>
+
+<section id="section_2" class="py-5 bg-light commsec">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10">
